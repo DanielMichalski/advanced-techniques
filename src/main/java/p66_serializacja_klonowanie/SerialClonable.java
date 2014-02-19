524be786ce8970ea6be8d3ebc12aac3e59e076c6
bakeraw2 <michalskidaniel2@gmail.com>
2014-02-20 00:40:46 +0100

    Poprawki

---
+++ src/main/java/p66_serializacja_klonowanie/SerialClonable.java
@@ -0,0 +1,27 @@
+package p66_serializacja_klonowanie;
+
+import java.io.*;
+
+/**
+ * Author: Daniel
+ */
+public class SerialClonable implements Cloneable, Serializable {
+    @Override
+    protected Object clone() {
+        try {
+            ByteArrayOutputStream bout = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bout);
+            oos.writeObject(this);
+            bout.close();
+
+            ByteArrayInputStream bin = new ByteArrayInputStream(bout.toByteArray());
+            ObjectInputStream in = new ObjectInputStream(bin);
+            Object object = in.readObject();
+            in.close();
+
+            return object;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
